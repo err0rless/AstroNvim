9e3419432fe052c1a3f46d7f4c0315057101fbea
hibyegoodbye <err0rless313@gmail.com>
2024-01-08 15:03:03 +0900

    personal setting

--- lua/astronvim/options.lua
+++ lua/astronvim/options.lua
@@ -52,13 +52,13 @@ local options = astronvim.user_opts("options", {
     -- AstroNvim specific global options
     max_file = { size = 1024 * 100, lines = 10000 }, -- set global limits for large files
     autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-    autopairs_enabled = true, -- enable autopairs at start
+    autopairs_enabled = false, -- disable autopairs at start
     cmp_enabled = true, -- enable completion at start
     codelens_enabled = true, -- enable or disable automatic codelens refreshing for lsp that support it
     diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     highlighturl_enabled = true, -- highlight URLs by default
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available)
-    inlay_hints_enabled = false, -- enable or disable LSP inlay hints on startup (Neovim v0.10 only)
+    inlay_hints_enabled = true, -- enable or disable LSP inlay hints on startup (Neovim v0.10 only)
     lsp_handlers_enabled = true, -- enable or disable default vim.lsp.handlers (hover and signature help)
     semantic_tokens_enabled = true, -- enable or disable LSP semantic tokens on startup
     ui_notifications_enabled = true, -- disable notifications (TODO: rename to  notifications_enabled in AstroNvim v4)
